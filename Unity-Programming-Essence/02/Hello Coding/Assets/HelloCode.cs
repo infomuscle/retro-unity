ef611c11a8e83bb3918c03d6ef38f151dcb3c402
infomuscle <infomuscle10@gmail.com>
2020-05-31 15:28:46 +0900

    04-9 마치며

--- Unity-Programming-Essence/02/Hello Coding/Assets/HelloCode.cs
+++ Unity-Programming-Essence/02/Hello Coding/Assets/HelloCode.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class HelloCode : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        // 캐릭터의 프로필을 변수로 만들기
-        string characterName = "라라";
-        char bloodType = 'A';
-        int age = 17;
-        float height = 168.3f;
-        bool isFemale = true;
+    void Start(){
+        int[] students = new int[5];
 
-        // 생성한 변수를 콘솔에 출력
-        Debug.Log("캐릭터 이름 : " + characterName);
-        Debug.Log("혈액형 : " + bloodType);
-        Debug.Log("나이 : " + age);
-        Debug.Log("키 : " + height);
-        Debug.Log("여성인가? : " + isFemale);
+        students[0] = 100;
+        students[1] = 90;
+        students[2] = 80;
+        students[3] = 70;
+        students[4] = 60;
 
+        for (int i = 0; i< students.Length; i++)
+        {
+            Debug.Log((i + 1) + "번 학생의 점수: " + students[i]);
+        }
     }
-
 }
